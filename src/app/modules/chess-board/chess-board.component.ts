b00ce603fd27b80a7af5301103ffd7fbd34afc51
proxza <proxza@gmail.com>
2024-07-26 21:05:32 +1000

    Add chessboard

---
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { ChessBoard } from '../../chess-logic/chess-board';
+import { Color, FENChar } from '../../chess-logic/models';
+
+@Component({
+  selector: 'app-chess-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './chess-board.component.html',
+  styleUrl: './chess-board.component.css',
+})
+export class ChessBoardComponent {
+  private chessBoard = new ChessBoard();
+  public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
+  public get playerColor(): Color {
+    return this.chessBoard.playerColor;
+  }
+}
