3d7c65622f2f4e5160ebcf705ea5d214cd3f1c39
proxza <proxza@gmail.com>
2024-08-25 14:33:50 +1000

    u

--- src/app/chess-logic/chess-board.ts
+++ src/app/chess-logic/chess-board.ts
@@ -287,6 +287,7 @@ export class ChessBoard {
   }
 
   public move(prevX: number, prevY: number, newX: number, newY: number, promotedPieceType: FENChar | null): void {
+    if (this._isGameOver) throw new Error('Game is over, you cant play move');
     if (!this.areCoordsValid(prevX, prevY) || !this.areCoordsValid(newX, newY)) return;
     const piece: Piece | null = this.chessBoard[prevX][prevY];
     if (!piece || piece.color !== this._playerColor) return;
